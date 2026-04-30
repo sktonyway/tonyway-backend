d1280a6cc8857349b5f64eb7075930c1c0699c9e
Sagar <Sktonyway@gmail.com>
2026-05-01 01:48:18 +0530

    better Db handling

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ import app from "./src/app.js";
 import connectDB from "./src/common/config/db.js";
 
 const port = process.env.port || 5000;
+const uri = process.env.mongodb_uri;
 
 const start = async () => {
-  await connectDB();
+  await connectDB(uri);
   app.listen(port, () => {
     console.log(`Server is running at ${port} smoothly.`);
   });
--- src/common/config/db.js
+++ src/common/config/db.js
@@ -1,8 +1,17 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.mongodb_uri);
-  console.log("MongoDB connected successfully at: ", conn.connection.host);
-};
+async function connectDB(uri) {
+  if (!uri) {
+    throw new Error("Connection String Required ");
+    process.exit(1);
+  }
+  try {
+    const conn = await mongoose.connect(process.env.mongodb_uri);
+    console.log("MongoDB connected successfully at: ", conn.connection.host);
+  } catch (error) {
+    console.log("MonggoDB connection Error", error);
+    process.exit(1);
+  }
+}
 
 export default connectDB;
